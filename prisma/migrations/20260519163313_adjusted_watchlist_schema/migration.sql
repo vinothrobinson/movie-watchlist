6a73425c4af4142a977d058ee683b32689ed3688
vinothrobinson <vinothrobinson03@gmail.com>
2026-05-21 11:27:55 -0400

    Adjusted structure

--- prisma/migrations/20260519163313_adjusted_watchlist_schema/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "WatchlistItem" ALTER COLUMN "updatedAt" DROP DEFAULT;
-
--- CreateIndex
-CREATE INDEX "WatchlistItem_userId_idx" ON "WatchlistItem"("userId");
-
--- CreateIndex
-CREATE INDEX "WatchlistItem_movieId_idx" ON "WatchlistItem"("movieId");
--- src/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { searchForMovie } from "../controllers/movieController.js";
-
-// Creating the router for the Movie route
-const router = express.Router();
-
-router.get("/search", searchForMovie);
-
-export default router;
