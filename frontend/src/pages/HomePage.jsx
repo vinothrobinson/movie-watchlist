09588dc07f2a3d6fe7d00fa07855e5ce2ed22716
vinothrobinson <vinothrobinson03@gmail.com>
2026-05-22 12:22:10 -0400

    Started front end home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <body className="bg-linear-to-r from-cyan-200 to-blue-200 font-sans">
+      <section className="space-y-5 text-center mx-auto">
+        <h1 className="text-6xl text-center font-semi-bold my-8">Home</h1>
+        <p className="max-w-3xl mx-auto text-wrap">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          quod dignissimos nostrum labore itaque nihil, ex eius voluptas
+          aspernatur atque?
+        </p>
+      </section>
+    </body>
   );
 }
