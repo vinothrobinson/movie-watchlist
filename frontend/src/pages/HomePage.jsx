aa7f3c52c76f5056789836c97ef44856111bbfcb
vinothrobinson <vinothrobinson03@gmail.com>
2026-05-24 12:06:42 -0400

    Created basic layout for the Home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,28 @@
+import phm from "../assets/phm.webp";
+
 export default function HomePage() {
   return (
-    <body className="bg-linear-to-r from-cyan-200 to-blue-200 font-sans">
-      <section className="space-y-5 text-center mx-auto">
-        <h1 className="text-6xl text-center font-semi-bold my-8">Home</h1>
-        <p className="max-w-3xl mx-auto text-wrap">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          quod dignissimos nostrum labore itaque nihil, ex eius voluptas
-          aspernatur atque?
-        </p>
+    <div className="bg-beige-200 font-['Inter']">
+      <section className="min-h-screen grid lg:grid-cols-2 grid-cols-1 items-center justify-items-center gap-12 px-8">
+        <div className="space-y-6 text-center max-w-l">
+          <h1 className="text-6xl font-semi-bold">CineLog</h1>
+          <p className="max-w-3xl text-lg text-wrap">
+            Welcome to CineLog! To get started click the "Log In" button. If you
+            are a new user, you will be able to easily create your new account.
+            Once signed in, you can search for movies you have seen and add them
+            to your "Watchlist". You will also be able to view your watchlist
+            and update the status and ratings of any of the movies you have
+            seen!
+          </p>
+          <button className="py-3 px-8 bg-maroon-500 hover:bg-maroon-200 text-white rounded-full transition">
+            Log In
+          </button>
+        </div>
+        <img
+          src={phm}
+          className="w-full max-w-2xl border-beige-500 border-8 rounded-2xl object-cover"
+        />
       </section>
-    </body>
+    </div>
   );
 }
